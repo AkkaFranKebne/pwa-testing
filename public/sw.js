56acc78c58b69f526afb652f09077005d6d27afb
AkkaFranKebne <ewawrobel@gmail.com>
2018-11-27 10:04:21 +0100

    parse request function

--- public/sw.js
+++ public/sw.js
@@ -17,12 +17,15 @@ var CACHE_STATIC_FILES = [
 ];
 
 function isInArray(string, array) {
-  for (var i = 0; i < array.length; i++) {
-    if (array[i] === string) {
-      return true;
-    }
+  var cachePath;
+  if (string.indexOf(self.origin) === 0) {
+    // request targets domain where we serve the page from (i.e. NOT a CDN)
+    console.log('matched ', string);
+    cachePath = string.substring(self.origin.length); // take the part of the URL AFTER the domain (e.g. after localhost:8080)
+  } else {
+    cachePath = string; // store the full request (for CDNs)
   }
-  return false;
+  return array.indexOf(cachePath) > -1;
 }
 
 // static cache of apps shell strategy
